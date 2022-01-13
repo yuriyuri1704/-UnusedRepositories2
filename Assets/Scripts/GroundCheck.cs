509f3e2118869ffaf9805994dc1f6438ede1f6f6
yuriyuri1704 <haruonma1117@gmail.com>
2022-01-13 17:38:38 +0900

    add:jumpMove

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -5,27 +5,69 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     private string groundTag = "Ground";
-    // Start is called before the first frame update
+    private bool isGround = false;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+    
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
 
     }
 
-    //OnTriggerEnter2D：Unity側から呼ばれるメソッド
+    public bool IsGround()
+    {
+        if(isGroundEnter || isGroundStay)
+        {
+            isGround = true;
+        }
+        else if (isGroundExit)
+        {
+            isGround = false;
+        }
+
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+
+        return isGround;
+    }
+
+    //Unity側から呼ばれるメソッド
     //親オブジェクトであるPlayerにRigidbody2Dがある条件より、呼び出される
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    //OnTriggerEnter2D：判定内に侵入した時
+    private void OnTriggerEnter2D(Collider2D collision) 
     {
         if (collision.tag == groundTag)
         {
-            Debug.Log("地面が判定に入りました");
+            isGroundEnter = true; //地面が判定内に入る：true
         }
 
 
     }
+
+    //OnTriggerStay2D:判定内に入り続けていた時
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundStay = true;
+        }
+    }
+
+    //OnTriggerExit2D：判定内から出た時
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundExit = true;
+        }
+
+    }
+
 }
